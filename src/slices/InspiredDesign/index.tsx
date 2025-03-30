33e2c1badb7036bad4bc1b50ad5e906d2782eb30
chrisbrody <chrisbrody87@gmail.com>
2025-03-30 13:59:13 -0400

    added a bit of styling for "Services" slice

--- src/slices/InspiredDesign/index.tsx
+++ src/slices/InspiredDesign/index.tsx
@@ -3,6 +3,8 @@ import { Content } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 
+import styles from "./InspiredDesign.module.css";
+
 /**
  * Props for `InspiredDesign`.
  */
@@ -19,14 +21,16 @@ const InspiredDesign: FC<InspiredDesignProps> = ({ slice }) => {
           data-slice-variation={slice.variation}
           className="p-6 bg-gray-100"
       >
-          {slice.primary.services?.map((item, index) => (
-              <div key={index} className="mt-4">
-                  <PrismicNextImage field={item.service_image} />
-                  <PrismicRichText field={item.service_header} />
-                  <PrismicRichText field={item.service_text} />
-                  <PrismicNextLink field={item.service_link} />
-              </div>
-          ))}
+          <div className={styles.serviceWrap}>
+              {slice.primary.services?.map((item, index) => (
+                  <div key={index} className="mt-4">
+                      <PrismicNextImage field={item.service_image} />
+                      <PrismicRichText field={item.service_header} />
+                      <PrismicRichText field={item.service_text} />
+                      <PrismicNextLink field={item.service_link} />
+                  </div>
+              ))}
+          </div>
       </section>
   );
 };
