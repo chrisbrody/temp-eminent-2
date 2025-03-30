50478fd6478e7855ab935aa5929536dc226b8d75
chrisbrody <chrisbrody87@gmail.com>
2025-03-30 13:10:53 -0400

    comment out header / seems to be the issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default async function RootLayout({
   return (
     <html lang="en" className={inter.variable}>
       <body className="overflow-x-hidden antialiased">
-        <Header />
+        {/*<Header />*/}
         {children}
         <PrismicPreview repositoryName={repositoryName} />
       </body>
