63c57488cf2f39c26457d389ee010f251c6eec3f
chrisbrody <chrisbrody87@gmail.com>
2025-05-08 13:26:53 -0400

    updated SectionHeader slice to hide eyebrow if there isnt one

--- src/slices/SectionHeader/index.tsx
+++ src/slices/SectionHeader/index.tsx
@@ -1,14 +1,18 @@
 // slices/SectionHeader/index.tsx
 import { FC } from "react";
-import type { Content } from "@prismicio/client";
-import type { SliceComponentProps } from "@prismicio/react";
+import { Content, isFilled } from "@prismicio/client";
+import { SliceComponentProps, PrismicText } from "@prismicio/react";
 
 export type SectionHeaderProps = SliceComponentProps<Content.SectionHeaderSlice>;
 
 const SectionHeader: FC<SectionHeaderProps> = ({ slice }) => {
     return (
         <div className="section-header text-center mt-10">
-            <h1>{slice.primary.eyebrow}</h1>
+            {'eyebrow' in slice.primary && isFilled.keyText(slice.primary.eyebrow) && (
+                <h1>
+                    <PrismicText field={slice.primary.eyebrow} />
+                </h1>
+            )}
             <h2>{slice.primary.title}</h2>
             <p>{slice.primary.cursivetext}</p>
         </div>
