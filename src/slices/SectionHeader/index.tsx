b475fd00c6b305c8910e6e953ac4d4aee90f15bf
chrisbrody <chrisbrody87@gmail.com>
2025-05-08 13:42:55 -0400

    updated SectionHeader to display h1 when the variant calls for it and filled out

--- src/slices/SectionHeader/index.tsx
+++ src/slices/SectionHeader/index.tsx
@@ -6,15 +6,21 @@ import { SliceComponentProps } from "@prismicio/react";
 export type SectionHeaderProps = SliceComponentProps<Content.SectionHeaderSlice>;
 
 const SectionHeader: FC<SectionHeaderProps> = ({ slice }) => {
+    const shouldShowEyebrow = slice.variation !== "sectionHeaderNoEyebrow";
+
     return (
         <div className="section-header text-center mt-10">
-            {'eyebrow' in slice.primary && isFilled.keyText(slice.primary.eyebrow) && (
-                <h1>
-                    {slice.primary.eyebrow}
-                </h1>
+            {shouldShowEyebrow && 'eyebrow' in slice.primary && isFilled.keyText(slice.primary.eyebrow) && (
+                <h1>{slice.primary.eyebrow}</h1>
             )}
-            <h2>{slice.primary.title}</h2>
-            <p>{slice.primary.cursivetext}</p>
+
+            {isFilled.keyText(slice.primary.title) && (
+                <h2>{slice.primary.title}</h2>
+            )}
+
+            {isFilled.keyText(slice.primary.cursivetext) && (
+                <p>{slice.primary.cursivetext}</p>
+            )}
         </div>
     );
 };
