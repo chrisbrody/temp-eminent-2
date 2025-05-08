1d1c95ba802dfc637d7bde7af040cc90486ce6db
chrisbrody <chrisbrody87@gmail.com>
2025-05-08 11:12:46 -0400

    fix error Binding element 'slice' implicitly has an 'any' type.

--- src/slices/SectionHeader/index.tsx
+++ src/slices/SectionHeader/index.tsx
@@ -1,24 +1,19 @@
 import { FC } from "react";
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import type { Content } from "@prismicio/client";
+import type { SliceComponentProps } from "@prismicio/react";
 
-/**
- * Props for `SectionHeader`.
- */
-export type SectionHeaderProps =
-  SliceComponentProps<Content.SectionHeaderSlice>;
+export type SectionHeaderProps = SliceComponentProps<Content.SectionHeaderSlice>;
 
-/**
- * Component for "SectionHeader" Slices.
- */
-const SectionHeader = ({ slice }) => {
-  return (
-      <div className="section-header text-center mt-10">
-          <h1>{slice.primary.eyebrow}</h1>
-          <h2>{slice.primary.title}</h2>
-          <p style={{ fontFamily: "'Herr Von Muellerhoff', cursive" }}>{slice.primary.cursivetext}</p>
-      </div>
-  )
-}
+const SectionHeader: FC<SectionHeaderProps> = ({ slice }) => {
+    return (
+        <div className="section-header text-center mt-10">
+            <h1>{slice.primary.eyebrow}</h1>
+            <h2>{slice.primary.title}</h2>
+            <p style={{ fontFamily: "'Herr Von Muellerhoff', cursive" }}>
+                {slice.primary.cursivetext}
+            </p>
+        </div>
+    );
+};
 
 export default SectionHeader;
